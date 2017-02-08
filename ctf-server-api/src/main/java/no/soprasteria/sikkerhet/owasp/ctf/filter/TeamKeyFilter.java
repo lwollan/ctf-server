3ff90abd34a51f84bb398f613d9b81b7038cc6ed
Lars-Erik Wollan <lwollan@online.no>
2017-02-08 09:33:05 +0100

    Sjekker team-key.

--- ctf-server-api/src/main/java/no/soprasteria/sikkerhet/owasp/ctf/filter/TeamKeyFilter.java
+++ ctf-server-api/src/main/java/no/soprasteria/sikkerhet/owasp/ctf/filter/TeamKeyFilter.java
@@ -1,13 +1,18 @@
 package no.soprasteria.sikkerhet.owasp.ctf.filter;
 
+import no.soprasteria.sikkerhet.owasp.ctf.ApplicationContext;
+import no.soprasteria.sikkerhet.owasp.ctf.service.TeamService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.Optional;
 
 @Provider
 @TeamKey
@@ -17,22 +22,20 @@ public class TeamKeyFilter implements ContainerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(BeskyttetFilter.class);
 
+    @Context
+    Application application;
+
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException {
         String teamKey = containerRequestContext.getHeaderString(X_TEAM_KEY);
 
-        if (teamKeyIsInvalid(teamKey)) {
+        TeamService teamService = ApplicationContext.get(application, TeamService.class);
+        Optional<String> teamName = teamService.getTeamName(teamKey);
+        if (!teamName.isPresent()) {
             logger.info("Ugyldig team key.");
             logger.info(" -- headere: {}.", containerRequestContext.getHeaders());
             containerRequestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).header("WWW-Authenticate", X_TEAM_KEY).build());
         }
     }
 
-    private static boolean teamKeyIsInvalid(String teamKey) {
-        if (teamKey == null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
--- ctf-server-api/src/test/java/no/soprasteria/sikkerhet/owasp/ctf/filter/TeamKeyFilterTest.java
+++ ctf-server-api/src/test/java/no/soprasteria/sikkerhet/owasp/ctf/filter/TeamKeyFilterTest.java
@@ -1,12 +1,16 @@
 package no.soprasteria.sikkerhet.owasp.ctf.filter;
 
 
+import no.soprasteria.sikkerhet.owasp.ctf.ApplicationContext;
+import no.soprasteria.sikkerhet.owasp.ctf.CtFApplication;
+import no.soprasteria.sikkerhet.owasp.ctf.service.TeamService;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.Optional;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -16,11 +20,16 @@ public class TeamKeyFilterTest {
 
     private TeamKeyFilter filter;
     private ContainerRequestContext request;
+    private Optional<String> teamKey;
 
     @Before
-    public void oppsett() {
+    public void oppsett() throws Exception {
         filter = new TeamKeyFilter();
         request = mock(ContainerRequestContext.class);
+        filter.application = new CtFApplication();
+
+        TeamService teamService = ApplicationContext.get(filter.application, TeamService.class);
+        teamKey = teamService.addNewTeam("team-a");
 
     }
 
@@ -40,5 +49,23 @@ public class TeamKeyFilterTest {
         filter.filter(request);
 
         verify(request).getHeaderString("X-TEAM-KEY");
+    }
+
+    @Test
+    public void skal_godta_teamkey_som_finnes_i_teamservice() throws IOException {
+        when(request.getHeaderString(anyString())).thenReturn(teamKey.get());
+
+        filter.filter(request);
+
+        verify(request, never()).abortWith(any(Response.class));
+    }
+
+    @Test
+    public void skal_ikke_godta_teamkey_som_ikke_finnes_i_teamservice() throws IOException {
+        when(request.getHeaderString(anyString())).thenReturn("some-other-key");
+
+        filter.filter(request);
+
+        verify(request).abortWith(any(Response.class));
     }
 }
