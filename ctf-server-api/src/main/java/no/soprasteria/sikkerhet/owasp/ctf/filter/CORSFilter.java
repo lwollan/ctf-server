a6035825d8b5aaf52e1c7626eb78a1a8847d1ece
Lars-Erik Wollan <lwollan@online.no>
2017-02-13 12:13:47 +0100

    Kanskje greit å la browsere sende x-team-key'en til api'en.

--- ctf-server-api/src/main/java/no/soprasteria/sikkerhet/owasp/ctf/filter/CORSFilter.java
+++ ctf-server-api/src/main/java/no/soprasteria/sikkerhet/owasp/ctf/filter/CORSFilter.java
@@ -12,7 +12,7 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
         containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers","origin, content-type, accept, authorization");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers","origin, content-type, accept, authorization, x-team-key");
         containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
