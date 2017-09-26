a7239b787e8fa96af1cb752b4a5d9261d38b86c5
Lars-Erik Wollan <lars-erik.wollan@bekk.no>
2017-09-26 22:37:34 +0200

    Fikset slik at teamnavn kan inneholde space og helst ikke script inection

--- ctf-server-api/src/main/java/no/soprasteria/sikkerhet/owasp/ctf/api/requestparameters/AlphaNumbericParameter.java
+++ ctf-server-api/src/main/java/no/soprasteria/sikkerhet/owasp/ctf/api/requestparameters/AlphaNumbericParameter.java
@@ -5,7 +5,7 @@ public abstract class AlphaNumbericParameter implements ValidatedInputParameter 
     public String value;
 
     public AlphaNumbericParameter(String value) {
-        if (value.matches("^\\w*$")) {
+        if (value.matches("^\\w+( \\w+)*$")) {
             this.value = value;
         } else {
             throw new InvalidParameterException(getParameterName());
--- ctf-server-api/src/test/java/no/soprasteria/sikkerhet/owasp/ctf/api/requestparameters/TeamNameParameterTest.java
+++ ctf-server-api/src/test/java/no/soprasteria/sikkerhet/owasp/ctf/api/requestparameters/TeamNameParameterTest.java
@@ -9,4 +9,18 @@ public class TeamNameParameterTest {
         new TeamNameParameter("-- insert");
     }
 
+    @Test
+    public void should_accept_teamname_with_space_in_it() {
+        new TeamNameParameter("Team Me");
+    }
+
+    @Test
+    public void should_accept_single_word_teamname() {
+        new TeamNameParameter("mememe");
+    }
+
+    @Test(expected = InvalidParameterException.class)
+    public void should_not_accept_script_tags_in_teamname() {
+        new TeamNameParameter("<script>alert('hepp')</script>");
+    }
 }
